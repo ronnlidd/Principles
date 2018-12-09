18eeb43b5122acc77c8a553dea860fb026577e21
Ronnlidd <david.ronnlid@gmail.com>
2018-12-09 23:00:13 +0100

    before jquery npm uninstall due to CDN hosting for performance

--- public/javascripts/toggleP.js
+++ public/javascripts/toggleP.js
@@ -1,5 +1,34 @@
 window.onload = function () {
-    $(".toggleAllSSP").click(function () {
+   
+  $(".toggleAllSSP").click(function () {
       $(".SSP").toggle();
     });
+
+    $(".toggleAllBtn").click(function () {
+      $(".toggleItem").toggle();
+    });
+
   };
+
+  function oneElmToggle(elm) {
+    const display = elm.nextSibling.style.display;
+    if (display === "none" || display === "") {
+      elm.nextSibling.style.display = "block";
+    }
+    else {
+      elm.nextSibling.style.display = "none";
+    }
+  }
+  
+  function oneChildToggle(elm) {
+    const display = elm.firstChild.style.display;
+    if (display === "none" || display === "") {
+      elm.firstChild.style.display = "block";
+    }
+    else {
+      elm.firstChild.style.display = "none";
+    }
+  }
+
+
+
